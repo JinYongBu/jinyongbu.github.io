5a931ed5870ef456eb0803463f27abd7a6078e69
jyb <13885084323@163.com>
2024-05-12 16:43:07 +0800

    little test of 'use client'

--- src/app/galleries/page.tsx
+++ src/app/galleries/page.tsx
@@ -1,7 +1,32 @@
-export default function Galleries(){
-    return(
+'use client'
+import React from 'react';
+import ReactECharts from 'echarts-for-react';
+
+export default function Galleries() {
+    const options = {
+        grid: { top: 8, right: 8, bottom: 24, left: 36 },
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        },
+        yAxis: {
+            type: 'value',
+        },
+        series: [
+            {
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                smooth: true,
+            },
+        ],
+        tooltip: {
+            trigger: 'axis',
+        },
+    };
+    return (
         <>
             <h1>Galleries</h1>
+            <ReactECharts option={options} />
         </>
     )
 }
